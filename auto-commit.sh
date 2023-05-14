41a77a746459ffbd9bbc6f1555ef2b17b4df54ab
Fandi Hasnur <fandi.hasnur10@gmail.com>
2023-05-14 23:56:51 +0800

    Update remote url to https

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git remote set-url origin git@github.com:fhasnur/auto-greening.git
+git remote set-url origin https://github.com/fhasnur/auto-greening.git
+
 git add .
 git commit -m "auto commit"
 git push origin HEAD
